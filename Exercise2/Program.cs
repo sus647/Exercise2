45fec9c23b1bab33c38c1095cc7e2c0ad8891636
Sushant <C0916102@mylambton.ca>
2024-02-12 10:26:30 -0500

    Exercise 2 final

---
+++ Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a whole number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("The number is positive and " + (number % 2 == 0 ? "even" : "odd"));
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("The number is negative and " + (number % 2 == 0 ? "even" : "odd"));
+        }
+        else
+        {
+            Console.WriteLine("The number is zero (and it is even)");
+        }
+    }
+}
